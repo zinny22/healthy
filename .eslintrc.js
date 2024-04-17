52c47e48160df621f28ea66ed5746c68d72b3304
zinny22 <hziny722@naver.com>
2024-04-17 23:18:50 +0900

    fix: eslint파일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': false,
+    'prettier/prettier': 0,
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-shadow': 'error',
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
