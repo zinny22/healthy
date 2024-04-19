4fdd13f35ee860fdb6c4b4685722c5a08ac7b526
zinny22 <hziny722@naver.com>
2024-04-20 01:51:38 +0900

    [BO]feat : 파이어베이스 연동

--- backoffice/src/App.tsx
+++ backoffice/src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { db } from "./firebase";
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 function App() {
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  console.log(user);
+  const getTest = async () => {
+    // db 뒤에 "techInfo"는 정보를 가져올 컬렉션 이름이다.
+    const query = await getDocs(collection(db, "item"));
+
+    query.forEach((doc) => {
+      console.log(doc.id, doc.data());
+    });
+  };
+
+  const signUpEmail = () => {
+    return createUserWithEmailAndPassword(auth, userId, userPw).then((e) =>
+      console.log(e)
+    );
+  };
+
+  const loginEmail = () => {
+    return signInWithEmailAndPassword(auth, userId, userPw).then((e) =>
+      console.log(e)
+    );
+  };
+
+  // 최초 마운트 시에 getTest import
+  useEffect(() => {
+    getTest();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={userId}
+        onChange={(e) => setUserId(e.currentTarget.value)}
+      />
+      <input
+        value={userPw}
+        onChange={(e) => setUserPw(e.currentTarget.value)}
+      />
+      <button onClick={() => loginEmail()}>submit</button>
     </div>
   );
 }
