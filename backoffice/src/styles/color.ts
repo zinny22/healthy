155163ed54b14656dc3e33709ec21198ceb736ae
zinny22 <hziny722@naver.com>
2024-04-23 21:08:16 +0900

    [BO]style: color추가

---
+++ backoffice/src/styles/color.ts
@@ -0,0 +1,23 @@
+const color = {
+  default100: "rgba(0, 112, 240, 1)",
+  default60: "rgba(0, 112, 240, 0.6)",
+  default50: "rgba(37, 145, 253, 0.5)",
+  default40: "rgba(37, 145, 253, 0.4)",
+  default20: "rgba(37, 145, 253, 0.2)",
+
+  //seconds
+  seconds100: "rgba(18, 161, 80, 1)",
+  seconds60: "rgba(18, 161, 80, 0.6)",
+  seconds50: "rgba(18, 161, 80, 0.5)",
+  seconds30: "rgba(24, 201, 100, 0.3)",
+  seconds20: "rgba(18, 161, 80, 0.2)",
+
+  //danger
+  danger100: "rgba(243,18, 96, 1)",
+  dander60: "rgba(243,18, 96, 0.6)",
+  danger50: "rgba(243, 18, 96, 0.5)",
+  danger30: "rgba(243, 18, 96, 0.3)",
+  danger20: "rgba(243,18, 96, 0.2)",
+};
+
+export default color;
