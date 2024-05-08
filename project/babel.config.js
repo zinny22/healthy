0dace530822f827c24b4dceaadb5ca7b5e5fede6
zinny22 <hziny722@naver.com>
2024-05-08 21:24:06 +0900

    [PJ] feat: 파이어베이스 안드로이드 연동

--- project/babel.config.js
+++ project/babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // react-native-dotenv
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env.local',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
